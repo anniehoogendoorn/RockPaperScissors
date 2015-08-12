3724e6630a334b3269c7b635dc96a1d4908c3120
Samantha Maturen & Annie Hoogendoorn <student@epicodus.com>
2015-08-12 11:08:34 -0700

    Update method to handle different capitalization in user input.

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -3,10 +3,10 @@
     {
         function playGame($user1, $user2)
         {
-            if($user1 == $user2) {
+            if (strtolower($user1) == strtolower($user2)) {
                 return "You have a tie!";
             }
-
+            var_dump($user1);
         }
     }
  ?>
