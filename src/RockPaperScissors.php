872124caf7716ff34beb93179d70efc60ed1d2a9
Samantha Maturen & Annie Hoogendoorn <student@epicodus.com>
2015-08-12 16:54:00 -0700

    Change input text to white.

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -24,56 +24,37 @@
                 }
             }
 
+            if ($player1 == $player2) {
+                return "You have a tie!";
 
-
-
-
-
-
-            //
-            //
-            // if ((in_array($player1, $player_options)) && (in_array($player2, $player_options))) {
-            //
-
-                // Both players enter the same input
-                if ($player1 == $player2) {
-                    return "You have a tie!";
-
-                // Player1 chooses rock
-                } elseif ($player1 == "rock") {
-                    if ($player2 == "paper") {
-                        return "Player two wins!";
-                    }
-                    else {
-                        // scissors
-                        return "Player one wins!";
-                    }
-
-                // Player1 chooses paper
-                } elseif ($player1 == "paper") {
-                    if ($player2 == "scissors") {
-                        return "Player two wins!";
-                    //$player2 == "rock"
-                    } else {
-                        return "Player two wins!";
-                    }
-
-                // Player1 chooses scissors
-                } else {
-                    if ($player2 == "rock") {
-                        return "Player two wins!";
-                    //$player2 chooses paper
-                    } else {
-                        return "Player one wins!";
-                    }
-
+            // Player1 chooses rock
+            } elseif ($player1 == "rock") {
+                if ($player2 == "paper") {
+                    return "Player two wins!";
+                }
+                else {
+                    // scissors
+                    return "Player one wins!";
                 }
 
-        }
+            // Player1 chooses paper
+            } elseif ($player1 == "paper") {
+                if ($player2 == "scissors") {
+                    return "Player two wins!";
+                //$player2 == "rock"
+                } else {
+                    return "Player two wins!";
+                }
 
-        //     } else {
-        //         return "Invalid input! Try again!";
-        //     }
-        // }
+            // Player1 chooses scissors
+            } else {
+                if ($player2 == "rock") {
+                    return "Player two wins!";
+                //$player2 chooses paper
+                } else {
+                    return "Player one wins!";
+                }
+            }
+        }
     }
  ?>
