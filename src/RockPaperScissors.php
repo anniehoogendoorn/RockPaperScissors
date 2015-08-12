bd65aaf8fb33e9eaed5f15187e98ed26913e8cea
Samantha Maturen & Annie Hoogendoorn <student@epicodus.com>
2015-08-12 11:36:54 -0700

    Set up structure for Player 1 choices

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -3,8 +3,29 @@
     {
         function playGame($user1, $user2)
         {
-            if (strtolower($user1) == strtolower($user2)) {
+            $player1 = strtolower($user1);
+            $player2 = strtolower($user2);
+
+            // Both players enter the same input
+            if ($player1 == $player2) {
                 return "You have a tie!";
+
+            // Player1 chooses rock
+            } elseif ($player1 = "rock") {
+                if ($player2 = "paper") {
+                    Something happens
+                }
+                else {
+                    // scissors
+                }
+
+            // Player1 chooses paper
+            } elseif ($player1 = "paper") {
+                Something
+            // Player1 chooses scissors
+            } else {
+                Something
+
             }
             var_dump($user1);
         }
